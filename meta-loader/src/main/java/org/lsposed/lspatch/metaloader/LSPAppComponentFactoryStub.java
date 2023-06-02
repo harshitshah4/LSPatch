1e2888c0a4b57c306688eab8b3453b1154f11698
Harshit <harshitshah4@gmail.com>
2023-06-02 15:49:02 +0530

    Handling duplicate System.load calls

--- meta-loader/src/main/java/org/lsposed/lspatch/metaloader/LSPAppComponentFactoryStub.java
+++ meta-loader/src/main/java/org/lsposed/lspatch/metaloader/LSPAppComponentFactoryStub.java
@@ -91,7 +91,13 @@ public class LSPAppComponentFactoryStub extends AppComponentFactory {
                 soPath = cl.getResource("assets/lspatch/so/" + libName + "/liblspatch.so").getPath().substring(5);
             }
 
-            System.load(soPath);
+            try {
+                System.load(soPath);
+            } catch (Throwable error) {
+                // Catching the error to handle duplicate loading of library
+                Log.e(TAG, "Failed to load liblsptach.so", error);
+            }
+
         } catch (Throwable e) {
             throw new ExceptionInInitializerError(e);
         }
